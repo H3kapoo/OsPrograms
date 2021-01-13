e55b4c32bd70ca9e0b904994d763033334b37bb8
H3kapoo <andrei.boje00@e-uvt.ro>
2021-01-13 15:49:33 +0200

    Fixed algorithm,log status added

--- Deadlock/deadlock.py
+++ Deadlock/deadlock.py
@@ -5,7 +5,7 @@ allocList = []
 pCount = 0
 availableRes = 0
 resolveList = []
-minAvailableRes = 0
+resolved = 0
 
 #Line 1 MAX
 #Line 2 ALLOC List
@@ -18,43 +18,47 @@ with open("../dlData.txt","r") as f:
 
 #Calculate need list
 for i in range(0,pCount):
-    minAvailableRes += maxList[i]-1
     needList.append(maxList[i] - allocList[i])
 needListCopy = needList.copy()
 
 #Minimum available resources so that the system will not go into deadlock
-#no matter the order
-availableRes = min(needList)   #Minim
+#Start at min(NEED_LIST)
+availableRes = min(needList)
 
-print("="*36)
-print(f"= Min initial available resources: {availableRes}")
+#run Banker's to find min resources needed
+while True:
+    avCopy = availableRes
+    print(f"Available resources {availableRes} attempt:")
+    for i in range(0,pCount):
+        #Find suitable process based on available res
+        for p in range(0,pCount):
+            if p+1 not in resolveList and needList[p] <= availableRes:
+                resolveList.append(p+1)
+                availableRes += allocList[p]
+                print(f"Process {p+1} needed {needList[p]} now scheduled. It released {allocList[p]}. Available res: {availableRes}")
+                
 
-#While we still have processes to allocate resources to, run Banker's
-iterCount=0
-while len(resolveList) != pCount:
-    if iterCount > pCount+2:
-        print("= No safe state found!")
+    if len(resolveList) == pCount:
+        print("DONE")
         break
-    #Find suitable process based on available res
-    for p in range(0,pCount):
-        if p+1 not in resolveList and needList[p] <= availableRes:
-            resolveList.append(p+1)
-            availableRes+=allocList[p]
-            needList[p] = 0
-            break    
-    iterCount+=1
+    print("INCREMENTING")
+    availableRes = avCopy+1
+    needList = needListCopy.copy()
+    resolveList.clear()
+            
 
 
 #Print output
-print("========= ALLOC === MAX === NEED ===")
+print("========= ALLOC === MAX === NEED ====")
 for i in range(0,pCount):
     print(f"= P{str(i+1).ljust(4)}|",end='')
     print(f"{str(allocList[i]).center(8)}|",end='')
     print(f"{str(maxList[i]).center(8)}|",end='')
-    print(f"{str(needListCopy[i]).center(8)} =")
+    print(f"{str(needListCopy[i]).center(8)}  =")
 
 print("="*14,"SAFE SEQ","="*13)
-print(resolveList)
+print(resolveList if len(resolveList) == pCount else f"None for available resources {avCopy}")
 print("="*15,"STATS","="*15)
-print(f"= Min value for total av. resources: {sum(needListCopy)-pCount+1}")
+print(f"Min av. res. after initial allocation: {availableRes - sum(allocList)}")
+print(f"Total min resources needed           : { availableRes}")
 print("="*37)
